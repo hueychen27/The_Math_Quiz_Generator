62fc98f4a2eb17e763323964042a0d4d6838cd4d
Da Person <hueychen27@outlook.com>
2022-12-15 19:46:04 -0800

    Made a better UI

--- math.js
+++ math.js
@@ -6,23 +6,44 @@ const mapping = {
     3: "*",
     4: "/"
 }
+let numbers = {
+    num: null,
+    operator: null,
+    num2: null,
+    equation: null,
+    answer: null
+}
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 function generateEquation() {
-    let number = randomNumber(-30, 30);
-    let operator = randomNumber(1, 4);
-    let number2 = randomNumber(-40, 50);
-    let equation;
-    let mathAnswer;
-    if (operator == 1) mathAnswer = number + number2;
-    if (operator == 2) mathAnswer = number - number2;
-    if (operator == 3) mathAnswer = number * number2;
-    if (operator == 4) mathAnswer = (number / number2).toFixed(2);
-    operator = mapping[`${operator}`]
-    equation = `${number} ${operator} ${number2}`;
-    displayEquation.innerText = equation;
-    console.log(mathAnswer);
+    numbers.num = randomNumber(-30, 30);
+    numbers.operator = randomNumber(1, 4);
+    numbers.num2 = randomNumber(-40, 50);
+    if ((numbers.num == 0 || numbers.num2 == 0) && numbers.operator == 4) generateEquation();
+    if (numbers.operator == 1) numbers.answer = numbers.num + numbers.num2;
+    if (numbers.operator == 2) numbers.answer = numbers.num - numbers.num2;
+    if (numbers.operator == 3) numbers.answer = numbers.num * numbers.num2;
+    if (numbers.operator == 4) numbers.answer = (numbers.num / numbers.num2).toPrecision(2);
+    numbers.operator = mapping[`${numbers.operator}`];
+    numbers.equation = `${numbers.num} ${numbers.operator} ${numbers.num2}`;
+    displayEquation.innerText = numbers.equation;
+    console.log(numbers.answer);
 }
 
 generateEquation();
+
+function submit() {
+    const results = document.getElementById("results");
+    if (answer.value == numbers.answer) {
+        console.log("correct")
+        results.innerText = "Correct!";
+        results.className = "show correct";
+        generateEquation();
+        return;
+    }
+    console.log("incorrect")
+    results.innerText = "Incorrect. The right answer is " + numbers.answer + ".";
+    results.className = "show incorrect";
+    generateEquation();
+}
